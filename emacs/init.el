b9c24ed0af2b5baa92244d52a0d8e62349c87d97
Josh Crompton <josh.crompton@gmail.com>
2013-09-05 18:23:50 +0800

    Speed up startup, don't display the splash screen.

--- emacs/init.el
+++ emacs/init.el
@@ -3,6 +3,14 @@
 ;; particular machine with a hostname.el file in the ~/.emacs.d
 ;; directory.
 
+;; Turn off mouse interface early in startup to avoid momentary display.
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; No splash screen.
+(setq inhibit-startup-message t)
+
 ;; Font configuration
 (if (eq window-system 'x)
     (set-default-font "Liberation Mono 10"))
