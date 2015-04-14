206b5b5a55a139d6e88bec48917dc77470f0afdf
Josh Crompton <jcrompton@infoxchange.org.au>
2015-04-14 11:34:51 +1000

    Enable using tramp-mode with docker containers.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -60,6 +60,33 @@
 (setq-default indent-tabs-mode nil)
 
 
+;; Attempt to be able to use tramp to access files on a docker container
+;; Open files in Docker containers like so: /docker:drunk_bardeen:/etc/passwd
+;; Note: this came from http://www.emacswiki.org/emacs/TrampAndDocker
+(require 'tramp)
+
+(push
+ (cons
+  "docker"
+  '((tramp-login-program "docker")
+    (tramp-login-args (("exec" "-it") ("%h") ("/bin/bash")))
+    (tramp-remote-shell "/bin/sh")
+    (tramp-remote-shell-args ("-i") ("-c"))))
+ tramp-methods)
+
+(defadvice tramp-completion-handle-file-name-all-completions
+  (around dotemacs-completion-docker activate)
+  "(tramp-completion-handle-file-name-all-completions \"\" \"/docker:\" returns
+    a list of active Docker container names, followed by colons."
+  (if (equal (ad-get-arg 1) "/docker:")
+      (let* ((dockernames-raw (shell-command-to-string "docker ps | perl -we 'use strict; $_ = <>; m/^(.*)NAMES/ or die; my $offset = length($1); while(<>) {substr($_, 0, $offset, q()); chomp; for(split m/\\W+/) {print qq($_:\n)} }'"))
+             (dockernames (cl-remove-if-not
+                           #'(lambda (dockerline) (string-match ":$" dockerline))
+                           (split-string dockernames-raw "\n"))))
+        (setq ad-return-value dockernames))
+    ad-do-it))
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
