8e82ae765a3cd0053fbe1e73fe904ce2d726674e
Josh Crompton <josh.crompton@gmail.com>
2017-05-16 09:10:45 +1000

    Install and configure markdown mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -78,6 +78,14 @@
   :config
   (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode)))
 
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Tweak the visual aspects of the UI. ;;
