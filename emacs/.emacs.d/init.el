2c382d08e4a1ad40ebc49a1e325af9e5f58f4a06
Josh Crompton <josh.crompton@gmail.com>
2021-02-16 15:43:03 +1300

    Add functions and keybindings to move lines up and down

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -385,6 +385,24 @@
    (sqlite . t)
    ))
 
+;; Move current line up or down
+(defun move-line-up ()
+  "Move up the current line."
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2)
+  (indent-according-to-mode))
+
+(defun move-line-down ()
+  "Move down the current line."
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(global-set-key [(meta up)]  'move-line-up)
+(global-set-key [(meta down)]  'move-line-down)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
